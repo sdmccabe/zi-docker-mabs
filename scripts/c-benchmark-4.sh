4bcbe5af5c1bb91a191655f0f0f4e6c488cdd0cb
Stefan McCabe <stefanmccabe@gmail.com>
2016-05-22 16:16:40 -0400

    Post-MABS extension, see mabs2016 branch for original

---
+++ scripts/c-benchmark-4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#first, make sure the program is compiled
+cd /models/c
+make
+cd /scripts/
+echo "threads,real,user,sys,memory" > results/experiment-4/c-benchmark.csv
+
+
+declare -a parameters=(1 2 4 5 8 10 16 20 25 32 40 50 80 100 125 200 250 400 500)
+for threads in ${parameters[@]}
+do
+for ((i=0; i<10; i++))
+do 
+{ /usr/bin/time -f "${threads},%e,%U,%S,%M" /models/c/zi-traders -n 1000000000 -a 10000000 -t $threads ; } 2>> results/experiment-4/c-benchmark.csv
+done
+done
---
+++ scripts/full-experiment4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./c-benchmark-4.sh
+./c-openmp-benchmark-4.sh
+#./clojure-benchmark-4.sh
+./cpp-benchmark-4.sh
+./go-benchmark-4.sh
+#./haskell-benchmark-4.sh
+./java-benchmark-new-4.sh
+./java-benchmark-old-4.sh
+./scala-benchmark-4.sh
+#./erlang-benchmark-4.sh
